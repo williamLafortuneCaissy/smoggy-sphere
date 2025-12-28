a13f26cf6c1b06e6099e3d4f3fec471f8baae7f8
williamLafortuneCaissy <33548003+williamLafortuneCaissy@users.noreply.github.com>
2025-12-27 19:04:59 -0500

    test fix image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://williamlafortunecaissy.github.io',
-  base: '/smoggy-sphere',
 });
